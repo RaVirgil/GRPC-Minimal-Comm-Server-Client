80d7027dafcf54367007bc847c8642990f264fbd
Radu Virgil Andrei <andreyradu33@gmail.com>
2020-04-11 17:42:03 +0300

    Client + Server for Homework1

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greet.GreetClient(channel);
+            Console.WriteLine("Whats your name? ");
+            string name = Console.ReadLine();
+            var request = new ClientInputRequest { Name = name };
+            Console.WriteLine(client.PrintClientInput(request));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GrpcServer/Services/GreetService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class GreetService : Greet.GreetBase
+    {
+        public override Task<EmptyMessage> PrintClientInput(ClientInputRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Hello "+request.Name+"!");
+            return Task.FromResult(new EmptyMessage());
+
+        }
+    }
+}
